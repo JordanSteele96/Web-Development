5a0c6056b21416c339b0f736a7db64da9fc5d589
jordan <jordan@DESKTOP-C3LIQA9>
2020-08-27 17:43:32 +0100

    -Added three buttons to the layout -Two of the buttons now change the data on the graph (weekly data/monthly data)

--- PetrolPrices/script.js
+++ PetrolPrices/script.js
@@ -1,17 +1,20 @@
-const yearArr = [];
-const priceArr = [];
+const weekArr = [];
+const WeeklyPrices = [];
+const monthlyPrices = [];
+const monthArr = [];
+let myChart;
 
 async function createCharts() {
-  await getData();
+  await setup();
   var ctx = document.getElementById("canvas").getContext("2d");
-  var myChart = new Chart(ctx, {
+  myChart = new Chart(ctx, {
     type: "line",
     data: {
-      labels: yearArr,
+      labels: weekArr,
       datasets: [
         {
           label: "Price",
-          data: priceArr,
+          data: WeeklyPrices,
           backgroundColor: ["rgba(255, 99, 132, 0.2)"],
           borderColor: ["rgba(255, 99, 132, 1)"],
           borderWidth: 1,
@@ -41,9 +44,66 @@ async function getData() {
   table.forEach((elt) => {
     const columns = elt.split(",");
     const date = columns[0];
-    yearArr.push(date);
+    weekArr.push(date);
     const price = columns[1];
-    priceArr.push(price);
+    WeeklyPrices.push(price);
   });
 }
+
+//This function averages the prices for each month (monthlyPrices)
+async function addingUp() {
+  await getData();
+  let sum = 0;
+  for (let counter = 1; counter <= WeeklyPrices.length; counter++) {
+    sum += parseFloat(WeeklyPrices[counter - 1]);
+
+    if (counter % 4 == 0 && !counter == 0) {
+      monthlyPrices.push(sum.toFixed(2) / 4);
+      sum = 0;
+    }
+  }
+}
+
+// This function sets the X axis for monthly prices (monthArr)
+function arrMonths() {
+  var months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  var year = 0;
+  for (let i = 0; i < 224; i++) {
+    if (i % 12 == 0) {
+      year++;
+    }
+    monthArr.push(months[i % months.length] + ` Year(${year})`);
+  }
+}
+
+function changeDataToWeekly() {
+  myChart.data.datasets[0].data = WeeklyPrices;
+  myChart.data.labels = weekArr;
+  myChart.update();
+}
+
+function changeDataToMonthly() {
+  myChart.data.datasets[0].data = monthlyPrices;
+  myChart.data.labels = monthArr;
+  myChart.update();
+}
+
+async function setup() {
+  await addingUp();
+}
+
+arrMonths();
 createCharts();
