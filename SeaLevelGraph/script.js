da5a1459c57ab969b0ad9911130ab84502c10ea7
jordan <jordan@DESKTOP-C3LIQA9>
2020-08-28 17:04:14 +0100

    commit

---
+++ SeaLevelGraph/script.js
@@ -0,0 +1,85 @@
+var dateMonthly = [];
+var monthlyDataGMSL = [];
+
+var dateYearly = [];
+var yearlyDataGMSL = [];
+
+async function createCharts() {
+  await setup();
+  var ctx = document.getElementById("canvas").getContext("2d");
+  myChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: dateMonthly,
+      datasets: [
+        {
+          label: "GMSL",
+          data: monthlyDataGMSL,
+          backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+          borderColor: ["rgba(255, 99, 132, 1)"],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            scaleLabel: {
+              display: true,
+              labelString: "Measurement (mm)",
+            },
+          },
+        ],
+      },
+    },
+  });
+}
+
+async function getMonthlyData() {
+  const response = await fetch("data/test2.csv");
+  const data = await response.text();
+
+  const table = data.split("\n");
+
+  table.forEach((elt) => {
+    const columns = elt.split(",");
+    const Time = columns[0];
+    dateMonthly.push(Time);
+    const monthlyData = columns[1];
+    monthlyDataGMSL.push(monthlyData);
+  });
+}
+
+async function getYearlyData() {
+  const response = await fetch("data/test3.csv");
+  const data = await response.text();
+
+  const table = data.split("\n");
+
+  table.forEach((elt) => {
+    const columns = elt.split(",");
+    const Time = columns[0];
+    dateYearly.push(Time);
+    const monthlyData = columns[1];
+    yearlyDataGMSL.push(monthlyData);
+  });
+}
+
+async function changeDataToMonthly() {
+  myChart.data.datasets[0].data = monthlyDataGMSL;
+  myChart.data.labels = dateMonthly;
+  myChart.update();
+}
+
+async function changeDataToYearly() {
+  myChart.data.datasets[0].data = yearlyDataGMSL;
+  myChart.data.labels = dateYearly;
+  myChart.update();
+}
+
+async function setup() {
+  await getYearlyData();
+  await getMonthlyData();
+}
+createCharts();
