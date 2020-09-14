4167c07f66c157fa6c2e9feb0b434b5e155f54ca
jordan <jordan@DESKTOP-C3LIQA9>
2020-09-14 12:15:19 +0100

    -Added a 3x3 grid that randomizes colours

---
+++ BrainTrainer/script.js
@@ -0,0 +1,42 @@
+let boxContainer = document.getElementById("boxContainer");
+let colors = ["red", "orange", "green", "purple", "blue"];
+let colorArray = []; //Array that stores colors in random order
+let colorToPick; // this is the color the user must guess within the grid
+let numberOfSquares = 9;
+
+function createGridItems() { //creates the grid 
+  for (let i = 0; i < numberOfSquares; i++) {
+    let div = document.createElement("div");
+    div.classList.add("box", i);
+    div.id = "box" + i;
+    div.onclick = function changeColor() { };
+    boxContainer.appendChild(div);
+  }
+}
+
+function randomColor() { //takes color from "colors" and puts them into new array
+  for (let i = 0; i < numberOfSquares; i++) {
+    colorArray.push(colors[Math.floor(Math.random() * 5)]);
+  }
+}
+
+function hideColors() {   // This hides the colors and picks a color the the user to use
+  for (let i = 0; i < numberOfSquares; i++) {
+    document.getElementById("box" + i).style.backgroundColor = " #444";
+    colorToPick = colorArray[i];
+    document.getElementById("idColor").style.backgroundColor = colorToPick;
+  }
+}
+
+
+function startGame() { // starts the game 1.makes array -> disables start btn-> loops over colorsArray and colors the tiles
+  randomColor();
+  document.getElementById("startBtn").disabled = true;
+  for (let i = 0; i < numberOfSquares; i++) {
+    document.getElementById("box" + i).style.backgroundColor = colorArray[i];
+
+  }
+  setTimeout(hideColors, 2000);
+}
+
+createGridItems();
