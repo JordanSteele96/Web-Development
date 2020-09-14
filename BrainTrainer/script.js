f7e007bbeb9249b6f5fd27e1fd639ad8771842e9
jordan <jordan@DESKTOP-C3LIQA9>
2020-09-14 14:15:33 +0100

    -User can now change the color of the boxes in the play phase

--- BrainTrainer/script.js
+++ BrainTrainer/script.js
@@ -7,13 +7,13 @@ let numberOfSquares = 9;
 function createGridItems() { //creates the grid 
   for (let i = 0; i < numberOfSquares; i++) {
     let div = document.createElement("div");
-    div.classList.add("box", i);
+    div.classList.add("box");
     div.id = "box" + i;
     div.onclick = function changeColor() { };
     boxContainer.appendChild(div);
   }
 }
-
+createGridItems();
 function randomColor() { //takes color from "colors" and puts them into new array
   for (let i = 0; i < numberOfSquares; i++) {
     colorArray.push(colors[Math.floor(Math.random() * 5)]);
@@ -28,6 +28,18 @@ function hideColors() {   // This hides the colors and picks a color the the use
   }
 }
 
+document.querySelectorAll('.box').forEach(item => { // Allows user to change the color of the boxes
+  item.addEventListener('click', event => {
+    if (item.style.backgroundColor !== colorToPick) {
+      item.style.backgroundColor = colorToPick;
+    }
+    else {
+      item.style.backgroundColor = "#444";
+    }
+
+  })
+})
+
 
 function startGame() { // starts the game 1.makes array -> disables start btn-> loops over colorsArray and colors the tiles
   randomColor();
@@ -39,4 +51,4 @@ function startGame() { // starts the game 1.makes array -> disables start btn-> 
   setTimeout(hideColors, 2000);
 }
 
-createGridItems();
+
