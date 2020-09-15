020349685b803f9017bdd9954d3e6dd61f647a84
jordan <jordan@DESKTOP-C3LIQA9>
2020-09-15 16:56:08 +0100

    -Formatting

--- BrainTrainer/script.js
+++ BrainTrainer/script.js
@@ -7,20 +7,20 @@ let numberOfSquares = 9;
 let userGuessArray = [];
 let usersScore = 0;
 document.getElementById("submitBtn").disabled = true;
+
 function createGridItems() {
   //creates the grid
   for (let i = 0; i < numberOfSquares; i++) {
     let div = document.createElement("div");
     div.classList.add("box");
     div.id = "box" + i;
-    div.onclick = function changeColor() { };
+    div.onclick = function changeColor() {};
     boxContainer.appendChild(div);
   }
 }
 createGridItems();
 
 function randomColor() {
-
   //takes color from "colors" and puts them into new array
   for (let i = 0; i < numberOfSquares; i++) {
     colorArray.push(colors[Math.floor(Math.random() * colors.length)]);
@@ -49,7 +49,6 @@ document.querySelectorAll(".box").forEach((item) => {
 });
 
 function startGame() {
-
   colorArray = [];
   userGuessArray = [];
   // starts the game 1.makes array -> disables start btn-> loops over colorsArray and colors the tiles
@@ -65,53 +64,53 @@ function startGame() {
 
 function createUsersArray() {
   for (let i = 0; i < numberOfSquares; i++) {
-    if (document.getElementById("box" + i).style.backgroundColor !== "darkslategrey") { // if the box doesnt equal darkslategrey
-      userGuessArray.push(document.getElementById("box" + i).style.backgroundColor); // adds the color of the box to the array
-
+    if (
+      document.getElementById("box" + i).style.backgroundColor !==
+      "darkslategrey"
+    ) {
+      // if the box doesnt equal darkslategrey
+      userGuessArray.push(
+        document.getElementById("box" + i).style.backgroundColor
+      ); // adds the color of the box to the array
     } else {
-      userGuessArray.push(undefined); //if the color is darkgrey use undefined 
+      userGuessArray.push(undefined); //if the color is darkgrey use undefined
     }
   }
 }
-
-
-
-
-
 
 function compareArray() {
   let tempScore = 0;
   let numberOfSameColor = 0;
   for (let i = 0; i < colorArray.length; i++) {
-    if (colorArray[i] === colorToPick) { // how many colorToPick in colorArray
+    if (colorArray[i] === colorToPick) {
+      // how many colorToPick in colorArray
       numberOfSameColor++;
     }
-    if (userGuessArray[i] === colorToPick && colorArray[i] === colorToPick) { // How many same colors 
+    if (userGuessArray[i] === colorToPick && colorArray[i] === colorToPick) {
+      // How many same colors
       tempScore++;
-
     }
   }
-  if (tempScore === numberOfSameColor) { // if they are equal then the user got the right answer
+  if (tempScore === numberOfSameColor) {
+    // if they are equal then the user got the right answer
     //What to do if the user got the right answer
-    score.style.color = "green"
+    score.style.color = "green";
     usersScore++;
     score.innerHTML = "Score:" + usersScore;
-    setInterval(function () { // Changed the color of the 
-      score.style.color = "black"
+    setInterval(function () {
+      // Changed the color of the
+      score.style.color = "black";
     }, 2500);
-
   } else {
-    score.style.color = "red"
+    score.style.color = "red";
     usersScore = 0;
     score.innerHTML = "Score: " + usersScore;
     //What to do if the user got the wrong answer
     setInterval(function () {
-      score.style.color = "black"
+      score.style.color = "black";
     }, 2500);
   }
-
 }
-
 function submit() {
   document.getElementById("idColor").style.backgroundColor = "darkslategrey";
   createUsersArray();
