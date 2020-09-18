ae207d7973eedf6857f2dd1ac3a4ce00c7076c81
jordan <jordan@DESKTOP-C3LIQA9>
2020-09-18 14:53:04 +0100

    -Added a responsive burger menu

--- create/script.js
+++ create/script.js
@@ -12,3 +12,31 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav-links");
+const closebtn = document.querySelector("#close");
+
+const navSlide = () => {
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+  });
+};
+
+const navSlide2 = () => {
+  closebtn.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+  });
+};
+
+navSlide();
+navSlide2();
+
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
