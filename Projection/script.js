3192b4f87a82fd441d202fe866b9769cbddf0e9d
jordan <jordan@DESKTOP-C3LIQA9>
2020-09-18 12:26:17 +0100

    -Fixed a bug where the animation for the burger menu wouldnt play when closed

--- Projection/script.js
+++ Projection/script.js
@@ -16,3 +16,12 @@ const navSlide2 = () => {
 
 navSlide();
 navSlide2();
+
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
